25b164466442d28ed87b2d7146895ddd0091fe2a
Navya2228 <s555193@nwmissouri.edu>
2022-10-26 21:17:04 -0500

    modified computation files

--- routes/computation.js
+++ routes/computation.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var x = Math.floor(Math.random() * 10);
-    var a = Math.atan2();
-    var b = Math.atanh();
-    var c = Math.cbrt();
-  res.render('computation', { title: 'Bonus Computation', x:x, a:a, b:b, c:c, d:d });
+    var y = Math.floor(Math.random() * 10);
+    var a = Math.atan2(x,y);
+    var b = Math.atanh(x);
+    var c = Math.cbrt(x);
+  res.render('computation', { title: 'Bonus Computation', x:x,y:y, a:a, b:b, c:c });
 });
 
 module.exports = router;
